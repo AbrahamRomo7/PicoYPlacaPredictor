bab205234ab302987334a71f767359b1e220a234
AbrahamRomo7 <rketya@gmail.com>
2024-09-19 17:22:49 -0500

    Add modular folder structure for project architecture

---
+++ pico_y_placa_predictor/license_plate_number/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LicensePlateNumberConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'license_plate_number'
---
+++ pico_y_placa_predictor/validation_of_road/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ValidationOfRoadConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'validation_of_road'
